a076fbfa0f01e0f3553021e3ac1c4c33d013efc2
Anagha Phirke <anagha@solvative.com>
2026-02-06 17:13:57 +0530

    bug resolved

--- src/api/axios.js
+++ src/api/axios.js
@@ -11,7 +11,7 @@ const api = axios.create({
 api.interceptors.response.use(
   res => res,
   err => {
-    if (err.response?.status == 401) {
+    if (err.response?.status === 401) {
       localStorage.removeItem('token');
       window.location.href = '/';
 
